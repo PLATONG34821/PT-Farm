eef32018c1261d02b2232b2a4f59cfa1eca8c37c
PLATONG98 <121702673+PLATONG34821@users.noreply.github.com>
2023-02-27 11:44:25 +0700

    Update PT-Farm.js

--- PT-Farm.js
+++ PT-Farm.js
@@ -2,6 +2,7 @@ ll.registerPlugin('PT-Farm', 'Safeguard Your Farm.', [1,0,0]);
 
 mc.listen("onFarmLandDecay", (pos, entity) => {
     if(entity.isPlayer()) {
+      entity.hurt(1)
       log(entity.name," Try To Break Farmland at ",pos)
       return false;
     }
